fc1832e5c0058e635e43fe5ea509633e672280c0
Frank Salazar <franksalazar82@gamil.com>
2016-01-13 19:03:29 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// declare variables
+var firstName = "frank";
+var lastName = "salazar";
+var fullName = "Frank Salazar";
+
+//
+var a=1;
+var b=2;
+var c=4;
+var d=2;
+
+var fourteen = (a + b + c) * d;
+//
+var a2=10;
+var b2=25;
+var c2=40;
+
+var average = (a2+b2+c2)/3
